c3af6e7274f016329ebfbd2224350ca400359a04
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-03-29 15:08:32 +0300

    if statements

--- ifStatements.py
+++ ifStatements.py
@@ -1,7 +1,32 @@
-# used to check for conditions
+# used to check for conditions and executes if the condition is met
 from variables import is_raining
 
 if is_raining:
-    print("carry mwavuli")
+    print("carry and umbrella")
 else:
     print("Apply sunscreen")
+
+# blood donation a person below 50kgs is considered as underweight and cannot donate blood
+body_weight = float(input("Enter your body weight in kgs: "))
+
+if body_weight <= 50:
+    print("You are underweight and cannot donate blood")
+else:
+    print("you can proceed to blood donation!")
+
+# grading system
+students_marks =int(input("Enter your marks: "))
+if students_marks<30:
+    print("Score :E")
+elif students_marks<=39:
+    print("score: D")
+elif students_marks<=49:
+    print("score: C")
+elif students_marks<=59:
+    print("Score: B")
+elif students_marks<=69:
+    print("Score: B+")
+elif students_marks>69:
+    print("score A")
+else:
+    print("invalid marks")
